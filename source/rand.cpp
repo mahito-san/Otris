bef9951b6bdbfd98407da8a9aa360a1c0df53ff4
saif <farhansaif488@gmail.com>
2020-09-04 15:24:47 +0600

    rand.cpp

---
+++ source/rand.cpp
@@ -0,0 +1,24 @@
+#include "../include/rand.h"
+#include <ctime>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+Rand::Rand()
+{
+    srand(unsigned(time(0)));//seed
+}
+
+Rand::randNum(int x,int y)
+{
+    int randNum;
+    double random(double,double); //function definition
+    randNum = int(random(x,y));
+    return randNum;
+}
+
+double random(double start,double end)
+{
+    return start+(end-start)*rand()/(RAND_MAX+1.0);//max random defined
+}
