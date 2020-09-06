3983bc01761ff11965b523d15ab00f08ffb97462
Mahi812 <51543190+Mahi812@users.noreply.github.com>
2020-09-06 10:12:09 +0600

    init

---
+++ include/game.h
@@ -0,0 +1,98 @@
+#include "graph.h"
+#include "rand.h"
+#include "score.h"
+
+enum MARK
+{
+    GAME_STOP=-1;
+    GAME_PAUSE=0;
+    GAME_RUN=1;
+
+};
+
+class Game{
+private:
+    int m_penal[24][17];
+    color m_color[24][17];
+    Context* m_graph;
+
+    Context* nextGraph;
+    MARK mark;
+
+    Score s;
+
+    //This will explore whether the next position is legal
+    bool recoverPanel();
+
+    //Whether it touches the bottom
+    bool isAttachBottom();
+
+    //Whether it touches the left
+    bool isAttachLeft();
+
+    //Whether it touches the right
+    bool isAttachRight();
+
+    //Get the block shape randomly
+    char getShape();
+
+    //Assign a value to the panel array with a block array
+    bool setPenal();
+
+    //After the square is moved, the remaining panel information must be erased
+    bool erasePenal();
+
+public:
+
+    int x;
+    int y;
+    //The position of the current square
+    //this value can be set only after the
+    // square has moved or rotated successfully
+
+    Game();
+
+    ~Game();
+
+    //How to create blocks randomly
+    void createCube();
+
+    //The method of movement
+    void move(int dir);
+
+    //Rotation method
+    void roll();
+
+    //Block stop
+    void stop();
+
+    //Erase full rows
+    void erase();
+
+    //After erasing the above graphics, the whole piece falls
+    void down(int level);
+
+	void printNextCube(Context* m_graph);
+
+	void gameInit();
+
+MARK getMark();
+void setMark( MARK );
+
+void printHelep();
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
